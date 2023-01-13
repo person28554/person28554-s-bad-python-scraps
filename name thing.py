8c680d78336ba1e8bb4a334edb9a04ff4a2c7954
person28554 <95857340+person28554@users.noreply.github.com>
2023-01-13 17:53:49 +0000

    Add files via upload

---
+++ name thing.py
@@ -0,0 +1,8 @@
+# this script was made in like 3 minutes so stfu
+# use idle for best results
+
+print ("What is your first name?")
+firstname = input()
+print ("What is your surname?")
+surname = input()
+print ("Hello, ", firstname, surname)
