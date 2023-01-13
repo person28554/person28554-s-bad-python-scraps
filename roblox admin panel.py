9e66f47851ce5008df3ad17820346dd32037b2c5
person28554 <95857340+person28554@users.noreply.github.com>
2023-01-13 17:52:50 +0000

    Add files via upload

---
+++ roblox admin panel.py
@@ -0,0 +1,10 @@
+# basic ass fucking program lololol
+# use IDLE or shit wont work very well (im too dumb to figure out how to use the cmd prompt looking shit)
+
+print ("use IDLE or this wont work very well!!!")
+print ("Welcome to Roblox Admin Panel")
+print ("Enter the password of fuck off")
+password = input()
+if password == int(1234):
+ print("Access Granted!")
+else: print ("FUCK OFF")
