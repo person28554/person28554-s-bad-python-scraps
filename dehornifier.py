11d22fccd6820dbf5a0021de1db04bce8565f17b
person28554 <95857340+person28554@users.noreply.github.com>
2023-03-14 23:31:59 +0000

    Add files via upload

---
+++ dehornifier.py
@@ -0,0 +1,18 @@
+#LEL
+
+print("Welcome to the Dehornifier™")
+print("You may find one of the outcomes a bit edgy.... woops\n")
+
+var1 = int(input("Are you horny rn? press 1 for yes, 2 for no\n"))
+
+if var1 == 2:
+    print("Good.")
+elif var1 == 1:
+    print("Do you want to take drastic measures?")
+    var2 = int(input("Press 1 for yes, 2 for no\n"))
+    if var2 == 1:
+        print("Watch gore videos")
+    else:
+        print("Well if that threat didnt make you dehornified™ then idk what will")
+
+input("Press enter to exit the program")
