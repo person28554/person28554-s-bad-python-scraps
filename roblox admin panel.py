7fc94963950c4846555e976ec09026ad1e776e9f
person28554 <95857340+person28554@users.noreply.github.com>
2023-03-14 23:31:21 +0000

    Update roblox admin panel.py

--- roblox admin panel.py
+++ roblox admin panel.py
@@ -1,10 +1,9 @@
 # basic ass fucking program lololol
-# use IDLE or shit wont work very well (im too dumb to figure out how to use the cmd prompt looking shit)
 
-print ("use IDLE or this wont work very well!!!")
 print ("Welcome to Roblox Admin Panel")
-print ("Enter the password of fuck off")
-password = input()
+password = input("Enter the password or fuck off\n")
 if password == int(1234):
  print("Access Granted!")
 else: print ("FUCK OFF")
+ 
+ input("press enter to quit")
